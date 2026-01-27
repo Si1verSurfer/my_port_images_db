b679e39ae062fdcbe36e576d7469d0d7b0b7c937
dir-Savage <basharrizq@Bashars-MacBook-Air.local>
2026-01-28 01:15:26 +0200

    ooo

---
+++ schema_view.py
@@ -0,0 +1,46 @@
+"""
+C2 server full stack with React js and golang backend :
+url : https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/C2_server_images/C2Server.png
+
+
+
+Haykl erealestate full stack development with React js and firebase & flutter mobile app :
+urls :
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/hayklApp_images/Screenshot%202026-01-28%20at%2012.27.56%E2%80%AFAM.png
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/hayklApp_images/%D8%A7%D9%84%D9%85%D9%81%D8%B6%D9%84%D8%A9%20copy.png
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/hayklApp_images/%D8%A8%D8%AF%D8%A7%D9%8A%D8%A9.png
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/hayklApp_images/%D8%AA%D8%B3%D8%AC%D9%8A%D9%84%20%D8%AF%D8%AE%D9%88%D9%84.png
+
+
+IEL flutter mobile app and odoo integrations :
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/IEL_images/Create%20Trip.png
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/IEL_images/Home.png
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/IEL_images/Shipment%20Details.png
+
+
+
+Suar App flutter mobile app and golang backend and react js admin panel and db integrations :
+
+urls :
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/quranApp_images/screen.png
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/quranApp_images/screen%202.png
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/quranApp_images/screen%203.png
+
+
+
+Talabaty flutter app : 
+app for ordering food from restaurants and tracking orders for user and restaurant app for managing orders and menus :
+urls : 
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/talabaty_images/Splash.png
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/talabaty_images/Main%20Screen.png
+https://github.com/Si1verSurfer/my_port_images_db/blob/main/projects/talabaty_images/Store.png
+
+
+
+
+
+"""
+
+
+
+
